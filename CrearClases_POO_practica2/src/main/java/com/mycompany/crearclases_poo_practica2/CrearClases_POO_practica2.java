f8608cabed9d175d0606143e417aece32234da06
amnunez <camishe>
2023-05-12 11:16:11 -0500

    POO-CLASES

---
+++ CrearClases_POO_practica2/src/main/java/com/mycompany/crearclases_poo_practica2/CrearClases_POO_practica2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crearclases_poo_practica2;
+
+/**
+ *
+ * @author Personal
+ */
+public class CrearClases_POO_practica2 {
+
+    //crear objetos
+    public static void main(String[] args) {
+        
+        Ninas ninas1 = new Ninas();
+        ninas1.nombre="Melisa";
+        ninas.apellido="Murillo";
+        ninas.edad=9;
+        ninas.color_cabello="negro";
+        ninas.estatura=130;
+        
+        System.out.println(ninas1.nombre);
+        System.out.println(ninas1.apellido);
+        System.out.println(ninas1.edad);
+        System.out.println(ninas1.color_cabello);
+        System-out.println(ninas1.estatura);
+        
+    }
+}
